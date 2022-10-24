4b6b91237feb2b90ad89b3b7cd042cf8d666974d
Vladyslav Byelozerskykh <vladorangeqwer@gmail.com>
2022-10-24 19:49:17 -0400

    Initial

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+module.exports = 
+{
+    //"URI": "mongodb://127.0.0.1/book_store"
+    //enter my own mongodb
+    //"URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://mongodb+srv://sweetpickle24:<PASSWORD>@comp229.cyvnzqs.mongodb.net/COMP229",
+    
+    "Secret": 'SomeSecret'
+}
+
